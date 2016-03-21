92c753cdbc62126f9558f44d638ee7e8ccb3d559
Okan Menevşeoğlu <okan.menevseoglu@boun.edu.tr>
2016-03-21 14:12:36 +0200

    The project is created
    The project is created in the Intellij.

---
+++ src/main/java/menevseoglu/okan/model/Member.java
@@ -0,0 +1,7 @@
+package menevseoglu.okan.model;
+
+/**
+ * Created by okanm on 21.03.2016.
+ */
+public class Member {
+}
---
+++ src/test/java/menevseoglu/okan/Swe573ApplicationTests.java
@@ -0,0 +1,18 @@
+package menevseoglu.okan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Swe573Application.class)
+@WebAppConfiguration
+public class Swe573ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
