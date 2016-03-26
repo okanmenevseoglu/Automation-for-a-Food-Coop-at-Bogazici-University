2aaa8467537eeb301784443163ac4889b3f683ab
Okan Menevşeoğlu <okan.menevseoglu@boun.edu.tr>
2016-03-27 01:12:24 +0200

    Initial Commit

--- src/main/java/menevseoglu/okan/Swe573Application.java
+++ src/main/java/menevseoglu/okan/Swe573Application.java
@@ -1,9 +1,11 @@
 package menevseoglu.okan;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Swe573Application {
 
 	public static void main(String[] args) {
