aef9db6eae4f20c6c5130f45a1936d9fca768e84
Okan Menevşeoğlu <okan.menevseoglu@boun.edu.tr>
2016-05-10 12:42:06 +0300

    Unit tests for the unit type service implementation is added and its controller is created.

---
+++ src/main/java/menevseoglu/okan/controller/UnitTypeController.java
@@ -0,0 +1,39 @@
+package menevseoglu.okan.controller;
+
+import menevseoglu.okan.model.UnitType;
+import menevseoglu.okan.service.UnitTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * Controller class that handles request and response methods of the unit type operations.
+ */
+@RestController
+@RequestMapping(value = "/unitTypes")
+public class UnitTypeController {
+
+    @Autowired
+    UnitTypeService unitTypeService;
+
+    @RequestMapping(value = {"", "/"}, method = RequestMethod.GET)
+    public Iterable<UnitType> getUnitTypes() {
+        return unitTypeService.getUnitTypes();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public UnitType getUnitType(@PathVariable("id") short id) {
+        return unitTypeService.getUnitType(id);
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public void addUnitType(@Valid @RequestBody UnitType unitType) {
+        unitTypeService.saveUnitType(unitType);
+    }
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
+    public void deleteUnitType(@PathVariable("id") short id) {
+        unitTypeService.deleteUnitType(id);
+    }
+}
---
+++ src/test/java/menevseoglu/okan/service/impl/UnitTypeServiceImplTest.java
@@ -0,0 +1,97 @@
+package menevseoglu.okan.service.impl;
+
+import menevseoglu.okan.model.UnitType;
+import menevseoglu.okan.repository.UnitTypeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * This class has the unit tests for the unit type service operations.
+ */
+public class UnitTypeServiceImplTest {
+
+    @Mock
+    UnitTypeRepository unitTypeRepository;
+
+    @InjectMocks
+    UnitTypeServiceImpl unitTypeService;
+
+    private List<UnitType> unitTypeList;
+
+    private UnitType unitType;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        unitTypeList = mock(List.class);
+        unitType = mock(UnitType.class);
+    }
+
+    /**
+     * @verifies invoke findAll method of unit type repository
+     * @see UnitTypeServiceImpl#getUnitTypes()
+     */
+    @Test
+    public void getUnitTypes_shouldInvokeFindAllMethodOfUnitTypeRepository() throws Exception {
+        unitTypeService.getUnitTypes();
+        verify(unitTypeRepository, times(1)).findAll();
+    }
+
+    /**
+     * @verifies return what unit type repository returns
+     * @see UnitTypeServiceImpl#getUnitTypes()
+     */
+    @Test
+    public void getUnitTypes_shouldReturnWhatUnitTypeRepositoryReturns() throws Exception {
+        when(unitTypeRepository.findAll()).thenReturn(unitTypeList);
+        assertEquals(unitTypeService.getUnitTypes(), unitTypeList);
+    }
+
+    /**
+     * @verifies invoke findOne method of unit type repository with given id
+     * @see UnitTypeServiceImpl#getUnitType(short)
+     */
+    @Test
+    public void getUnitType_shouldInvokeFindOneMethodOfUnitTypeRepositoryWithGivenId() throws Exception {
+        unitTypeService.getUnitType(anyShort());
+        verify(unitTypeRepository, times(1)).findOne(anyShort());
+    }
+
+    /**
+     * @verifies return what unit type repository returns
+     * @see UnitTypeServiceImpl#getUnitType(short)
+     */
+    @Test
+    public void getUnitType_shouldReturnWhatUnitTypeRepositoryReturns() throws Exception {
+        when(unitTypeRepository.findOne(anyShort())).thenReturn(unitType);
+        assertEquals(unitTypeService.getUnitType((short) 1), unitType);
+    }
+
+    /**
+     * @verifies invoke save method of unit type repository
+     * @see UnitTypeServiceImpl#saveUnitType(menevseoglu.okan.model.UnitType)
+     */
+    @Test
+    public void saveUnitType_shouldInvokeSaveMethodOfUnitTypeRepository() throws Exception {
+        unitTypeService.saveUnitType(unitType);
+        verify(unitTypeRepository, times(1)).save(unitType);
+    }
+
+    /**
+     * @verifies invoke delete method of unit type repository with given id
+     * @see UnitTypeServiceImpl#deleteUnitType(short)
+     */
+    @Test
+    public void deleteUnitType_shouldInvokeDeleteMethodOfUnitTypeRepositoryWithGivenId() throws Exception {
+        unitTypeService.deleteUnitType(anyShort());
+        verify(unitTypeRepository, times(1)).delete(anyShort());
+    }
+}
