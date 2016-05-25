335304216ed9cfe1e7df733b043e65829cc21243
Okan Menevşeoğlu <okan.menevseoglu@boun.edu.tr>
2016-05-25 16:31:28 +0300

    Auth directory is updated. Some bugs are cleared.

--- src/main/resources/static/app/auth/auth.js
+++ src/main/resources/static/app/auth/auth.js
@@ -42,7 +42,7 @@ register.config(function ($stateProvider) {
         })
 });
 
-register.service("AuthService", ['$http', 'baseUrl', function ($http, baseUrl) {
+register.service('AuthService', ['$http', 'baseUrl', function ($http, baseUrl) {
 
     var membersUrl = baseUrl + '/members';
 
@@ -55,13 +55,13 @@ register.service("AuthService", ['$http', 'baseUrl', function ($http, baseUrl) {
     };
 
     this.getMemberByEmail = function (memberId) {
-        return $http.get(membersUrl + "/byEmail/" + memberId + "/");
+        return $http.get(membersUrl + '/byEmail/' + memberId + '/');
     };
 
     this.login = function (credentials) {
         var header = credentials ? {
-            Authorization: "Basic "
-            + btoa(credentials.username + ":" + credentials.password)
+            Authorization: 'Basic '
+            + btoa(credentials.username + ':' + credentials.password)
         } : {};
         return $http.get(membersUrl + '/user', {headers: header});
     };
@@ -71,7 +71,12 @@ register.service("AuthService", ['$http', 'baseUrl', function ($http, baseUrl) {
     };
 }]);
 
-register.controller('RegisterCtrl', ['$scope', 'AuthService', 'Notification', function ($scope, AuthService, Notification) {
+register.controller('RegisterCtrl', ['$scope', '$timeout', 'AuthService', 'Notification', function ($scope, $timeout, AuthService, Notification) {
+
+    AuthService.getMemberType('ROLE_MEMBER')
+        .then(function (response) {
+            $scope.memberType = response.data;
+        });
 
     $scope.memberData = {
         firstName: '',
@@ -84,92 +89,92 @@ register.controller('RegisterCtrl', ['$scope', 'AuthService', 'Notification', fu
         city: '',
         postalCode: '',
         phoneNumber: '',
-        memberType: {
-            id: 4
-        }
+        memberType: {}
     };
 
     $scope.submitForm = function (valid) {
-
         if (valid) {
+            $scope.memberData.memberType.id = $scope.memberType.id;
             AuthService.register($scope.memberData)
                 .success(function () {
-                    console.log($scope.memberData);
-                    Notification.success("Kayıt Başarılı!");
+                    Notification.success('Kayıt Başarılı!');
+                    $timeout(function () {
+                        window.location.reload()
+                    }, 500);
                 })
                 .error(function () {
-                    console.log($scope.memberData);
-                    Notification.error("Kayıt Başarısız!");
+                    Notification.error('Kayıt Başarısız!');
                 });
         }
     };
-
 }]);
 
-register.controller('LoginCtrl', ['$scope', '$cookies', '$timeout', 'AuthService', 'Notification', function ($scope, $cookies, $timeout, AuthService, Notification) {
+register.controller('LoginCtrl', ['$scope', '$state', '$cookies', '$timeout', 'AuthService', 'Notification', function ($scope, $state, $cookies, $timeout, AuthService, Notification) {
 
     $scope.credentials = {
-        username: "",
-        password: ""
+        username: '',
+        password: ''
     };
 
     $scope.onLogin = function () {
         AuthService.login($scope.credentials).success(function () {
             AuthService.getMemberByEmail($scope.credentials.username)
                 .then(function (response) {
-                    Notification.success("Giriş Başarılı!");
-                    $cookies.put("id", response.data.id);
-                    $cookies.put("firstName", response.data.firstName);
-                    $cookies.put("lastName", response.data.lastName);
-                    $cookies.put("email", response.data.email);
-                    $cookies.put("gender", response.data.gender);
-                    $cookies.put("idNumber", response.data.idNumber);
-                    $cookies.put("address", response.data.address);
-                    $cookies.put("city", response.data.city);
-                    $cookies.put("postalCode", response.data.postalCode);
-                    $cookies.put("phoneNumber", response.data.phoneNumber);
-                    $cookies.put("memberType", response.data.memberType.name);
-                    $cookies.put("registerTime", response.data.registerTime);
-                    $cookies.put("authenticated", true);
+                    Notification.success('Giriş Başarılı!');
+                    $cookies.put('id', response.data.id);
+                    $cookies.put('firstName', response.data.firstName);
+                    $cookies.put('lastName', response.data.lastName);
+                    $cookies.put('email', response.data.email);
+                    $cookies.put('gender', response.data.gender);
+                    $cookies.put('idNumber', response.data.idNumber);
+                    $cookies.put('address', response.data.address);
+                    $cookies.put('city', response.data.city);
+                    $cookies.put('postalCode', response.data.postalCode);
+                    $cookies.put('phoneNumber', response.data.phoneNumber);
+                    $cookies.put('memberType', response.data.memberType.name);
+                    $cookies.put('registerTime', response.data.registerTime);
+                    $cookies.put('authenticated', true);
                     $timeout(function () {
-                        window.location.reload()
+                        window.location.reload();
+                        $state.go('home', {}, {reload: true});
                     }, 500);
                 });
         })
             .error(function () {
-                Notification.success("Giriş Başarısız!");
-                $cookies.put("authenticated", false);
+                Notification.success('Giriş Başarısız!');
+                $cookies.put('authenticated', false);
                 $timeout(function () {
-                    window.location.reload()
+                    window.location.reload();
                 }, 500);
             })
     };
 }]);
 
 register.controller('AuthCtrl', ['$scope', '$cookies', function ($scope, $cookies) {
-    $scope.memberType = $cookies.get("memberType");
-    $scope.authenticated = $cookies.get("authenticated");
+    $scope.memberType = $cookies.get('memberType');
+    $scope.authenticated = $cookies.get('authenticated');
+
+
 }]);
 
-register.controller('LogoutCtrl', ['$scope', '$cookies', 'AuthService', 'Notification', function ($scope, $cookies, AuthService, Notification) {
-    AuthService.logout()
-        .then(function () {
-            Notification.success("Çıkış Başarılı!");
-            $cookies.remove('id');
-            $cookies.remove('firstName');
-            $cookies.remove('lastName');
-            $cookies.remove('email');
-            $cookies.remove('gender');
-            $cookies.remove('idNumber');
-            $cookies.remove('address');
-            $cookies.remove('city');
-            $cookies.remove('postalCode');
-            $cookies.remove('phoneNumber');
-            $cookies.remove('memberType');
-            $cookies.remove('registerTime');
-            $cookies.remove('authenticated');
-            $timeout(function () {
-                window.location.reload()
-            }, 500);
-        });
+register.controller('LogoutCtrl', ['$scope', '$state', '$cookies', '$timeout', function ($scope, $state, $cookies, $timeout) {
+    (function () {
+        $cookies.remove('id');
+        $cookies.remove('firstName');
+        $cookies.remove('lastName');
+        $cookies.remove('email');
+        $cookies.remove('gender');
+        $cookies.remove('idNumber');
+        $cookies.remove('address');
+        $cookies.remove('city');
+        $cookies.remove('postalCode');
+        $cookies.remove('phoneNumber');
+        $cookies.remove('memberType');
+        $cookies.remove('registerTime');
+        $cookies.remove('authenticated');
+        $timeout(function () {
+            window.location.reload();
+            $state.go('home', {}, {reload: true});
+        }, 500);
+    })();
 }]);
